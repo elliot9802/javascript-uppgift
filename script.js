6592a3776074173059c48e33f7d4b3b69a088d85
Elliot Segerlind <elliot@segerlind.se>
2021-12-02 17:50:10 +0100

    Changes

--- script.js
+++ script.js
@@ -21,6 +21,16 @@
     let triangleTwo = (16.5 * 20.3) / 2
     // console.log(triangleTwo)
 
+    if (triangleOne > triangleTwo) {
+        console.log(`Triangle One's area is greater than triangle two. Triangle One's area is ${triangleOne}`)
+    } else if (triangleTwo > triangleOne) {
+        console.log(`Triangle Two's Area is greater than Triangle One. Triangle Two's area is ${triangleTwo}`)
+    } else if (triangleOne === triangleTwo) {
+        console.log(`Triangle One and Two's area are the same.`)
+    } else {
+        console.log (`Invalid`)
+    }
+
     // Triangle One has an area of 65 cm2 and Triangle Two 
     // has an area of 167,475 cm2. 
     // The triangle with the biggest area is Triangle Two.
@@ -34,7 +44,17 @@
     let triangleFour = (20.3 * 16.5) / 2
     // console.log(triangleFour)
 
-    // Triangle One has an area of 167,475 cm2 and Triangle Two 
+    if (triangleThree > triangleFour) {
+        console.log(`Triangle Three's area is greater than triangle Four. Triangle Three's area is ${triangleThree}`)
+    } else if (triangleFour > triangleThree) {
+        console.log(`Triangle Four's Area is greater than Triangle Three. Triangle Four's area is ${triangleFour}`)
+    } else if (triangleThree === triangleFour) {
+        console.log(`Triangle Three and Four's area are the same.`)
+    } else {
+        console.log (`Invalid`)
+    }
+
+    // Triangle Three has an area of 167,475 cm2 and Triangle Four 
     // has an area of 167,475 cm2. 
     // The triangle with the biggest area is neither they are equal.
 
@@ -46,14 +66,23 @@
     let triangleSix = (9.3 * 12.4) / 2
     // console.log (triangleSix)
 
+    if (triangleFive > triangleSix) {
+        console.log(`Triangle Five's area is greater than triangle Six. Triangle Five's area is ${triangleFive}`)
+    } else if (triangleSix > triangleFive) {
+        console.log(`Triangle Six's Area is greater than Triangle Five. Triangle Six's area is ${triangleSix}`)
+    } else if (triangleFive === triangleSix) {
+        console.log(`Triangle Five and Six's area are the same.`)
+    } else {
+        console.log (`Invalid`)
+    }
 
     // Triangle One has an area of 21,84 cm2 and Triangle Five 
     // has an area of 57,66 cm2. 
     // The triangle with the biggest area is Triangle Six.
 
     // * ANSWER TO THE QUESTION:
-    // The reason we need to use parantheses in this case is because of the priority order. We need to multiply
-    // before we divide in these cases and dividing is prioritised before multiplying in JavaScript.
+    // The reason we need to use parantheses in this case is because of the priority order that goes from left and right
+    //  and in this case I didnt' see any difference with parantheses but in bigger equations there might be issues.
 // ---------------------------------------------------------------------------------
 
 // Print the total area for each triangle together with the result 
